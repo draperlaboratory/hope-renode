6b1e7f115916d258bdb9b7db9db17aaf83c84cde
Eric Missimer <emissimer@draper.com>
2018-07-10 11:33:13 -0400

    Updated renode script to use relative path to find Renode.exe

--- tools/packaging/linux/renode.sh
+++ tools/packaging/linux/renode.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-mono $MONO_OPTIONS /opt/renode/bin/Renode.exe "$@"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mono $MONO_OPTIONS $SCRIPT_DIR/../renode/bin/Renode.exe "$@"
